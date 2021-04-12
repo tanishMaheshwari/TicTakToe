41d95cc4b5043c8f0631bdce9b972805224f5ba4
tanishMaheshwari <75633572+tanishMaheshwari@users.noreply.github.com>
2021-04-12 16:03:45 +0530

    Menu Update(Graphics Only)

--- GameMain.py
+++ GameMain.py
@@ -1,5 +1,9 @@
 '''
     Things to do
+    
+    main menu
+        Graphic part done
+    
     play agaain
     
     
@@ -29,70 +33,71 @@ whoWins = ''
 whereWin = ''
 
 def drawGameState(screen, gs):
-    screen.blit(Board, (0, 0))
-    #screen.blit(DiagonalLine1, (0, 0))
 
-    #Display the Board state
+    if scene == 1:
 
-    for row in range(3):
-        for col in range(3):
-            if gs.board[row][col] == 'X':
-                screen.blit(X, (row * sqSize + OFFSET, col * sqSize + OFFSET))
-            elif gs.board[row][col] == 'O':
-                screen.blit(O, (row * sqSize + OFFSET, col * sqSize + OFFSET))
+        screen.blit(Board, (0, 0))
+
+        for row in range(3):
+            for col in range(3):
+                if gs.board[row][col] == 'X':
+                    screen.blit(X, (row * sqSize + OFFSET, col * sqSize + OFFSET))
+                elif gs.board[row][col] == 'O':
+                    screen.blit(O, (row * sqSize + OFFSET, col * sqSize + OFFSET))
 
 
-    whoWins = gs.checkWin(gs.board)[0]
-    whereWin = gs.checkWin(gs.board)[1]
+        whoWins = gs.checkWin(gs.board)[0]
+        whereWin = gs.checkWin(gs.board)[1]
 
-    #Test drawing
-    
-    #screen.blit(VerticalLine, (1, sqSize * 2 + OFFSET * 3))
-    
-    #screen.blit(HorizontalLine, (sqSize * 0 + OFFSET * 3,0))
+        #Test drawing
 
-    '''
-        NOTE
-        HORIZONTAL and  VERTICAL are interchanged
 
-    '''
-    if whoWins != '-':
-        if whereWin == '1':
-            screen.blit(HorizontalLine, (sqSize * 0 + OFFSET * 3,0))
-            print("Horizontal 1")
+        '''
+            NOTE
+            HORIZONTAL and  VERTICAL are interchanged
+        '''
+        if whoWins != '-':
+            if whereWin == '1':
+                screen.blit(HorizontalLine, (sqSize * 0 + OFFSET * 3,0))
+                print("Horizontal 1")
+            elif whereWin == '2':
+                screen.blit(HorizontalLine, (sqSize * 1 + OFFSET * 3,0))
+                print("Horizontal 2")
+            elif whereWin == '3':
+                screen.blit(HorizontalLine, (sqSize * 2 + OFFSET * 3,0))
+                print("Horizontal 3")
+            elif whereWin == '4':
+                screen.blit(VerticalLine, (1, sqSize * 0 + OFFSET * 3))
+                print("vertical 1")
+            elif whereWin == '5':
+                screen.blit(VerticalLine, (1, sqSize * 1 + OFFSET * 3))
+                print("Vertical 2")
+            elif whereWin == '6':
+                screen.blit(VerticalLine, (1, sqSize * 3 + OFFSET * 3))
+                print("Vertical 3")
+            elif whereWin == '7':
+                screen.blit(DiagonalLine2, (0, 0))
+                print("Diagonal 2")
+            elif whereWin == '8':
+                screen.blit(DiagonalLine1, (0, 0))
             
-        elif whereWin == '2':
-            screen.blit(HorizontalLine, (sqSize * 1 + OFFSET * 3,0))
-            print("Horizontal 2")
-        elif whereWin == '3':
-            screen.blit(HorizontalLine, (sqSize * 2 + OFFSET * 3,0))
-            print("Horizontal 3")
-        elif whereWin == '4':
-            screen.blit(VerticalLine, (1, sqSize * 0 + OFFSET * 3))
-            print("vertical 1")
-        elif whereWin == '5':
-            screen.blit(VerticalLine, (1, sqSize * 1 + OFFSET * 3))
-            print("Vertical 2")
-        elif whereWin == '6':
-            screen.blit(VerticalLine, (1, sqSize * 3 + OFFSET * 3))
-            print("Vertical 3")
-        elif whereWin == '7':
-            screen.blit(DiagonalLine2, (0, 0))
-            print("Diagonal 2")
-        elif whereWin == '8':
-            screen.blit(DiagonalLine1, (0, 0))
+        '''
+        if whoWins == 'X':
+            screen.fill((238,238,210))
+            screen.blit(resultMessageX, (sqSize - OFFSET , sqSize + OFFSET * 2))
+        elif whoWins == 'O':
+            screen.fill((238,238,210))
+            screen.blit(resultMessageO, (sqSize - OFFSET, sqSize + OFFSET * 2))
+        elif whoWins == 'D':
+            screen.fill((238,238,210))
+            screen.blit(resultMessageDraw, (sqSize - OFFSET, sqSize + OFFSET * 2))
+        '''
+    elif scene == 0:
+        screen.fill((238, 238, 210))
+        screen.blit(menuTitleText, (sqSize // 2 - OFFSET, OFFSET * 2 ))
+        screen.blit(PlayButton, (sqSize + OFFSET - sqSize // 2, sqSize + OFFSET))
+        screen.blit(QuitButton, (sqSize + OFFSET + sqSize // 2, sqSize + OFFSET))
         
-    '''
-    if whoWins == 'X':
-        screen.fill((238,238,210))
-        screen.blit(resultMessageX, (sqSize - OFFSET , sqSize + OFFSET * 2))
-    elif whoWins == 'O':
-        screen.fill((238,238,210))
-        screen.blit(resultMessageO, (sqSize - OFFSET, sqSize + OFFSET * 2))
-    elif whoWins == 'D':
-        screen.fill((238,238,210))
-        screen.blit(resultMessageDraw, (sqSize - OFFSET, sqSize + OFFSET * 2))
-    '''
 
 
 
@@ -101,9 +106,13 @@ screen = p.display.set_mode((WIDTH, HEIGHT))
 p.display.set_caption("Tic Tac Toe by Tanish M.")
 
 resultMessageFont = p.font.SysFont(None, 100) 
-resultMessageX = resultMessageFont.render('X Wins', False,  (66, 245, 230))
-resultMessageO = resultMessageFont.render('O Wins', False,  (255, 0, 0))
-resultMessageDraw = resultMessageFont.render('Draw', False,  (0, 0, 0))
+menuFont = p.font.Font(r'D:\Programming\Python\IDLE\TicTakToe\Font\PermanentMarker-Regular.ttf', 72)
+
+menuTitleText = menuFont.render('TicTacToe', True, (0, 15, 155))
+
+resultMessageX = resultMessageFont.render('X Wins', True,  (66, 245, 230))
+resultMessageO = resultMessageFont.render('O Wins', True,  (255, 0, 0))
+resultMessageDraw = resultMessageFont.render('Draw', True,  (0, 0, 0))
 clock = p.time.Clock()
 screen.fill((238,238,210))
 
@@ -115,10 +124,15 @@ xToMove = True
 
 X = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\X.png"), (sqSize - (OFFSET * 2), sqSize - (OFFSET * 2)))
 O = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\O.png"), (sqSize - (OFFSET * 2), sqSize - (OFFSET * 2)))
+
 DiagonalLine1 = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\DiagonalLine1.png"), (sqSize * 3, sqSize * 3))
 DiagonalLine2 = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\DiagonalLine2.png"), (sqSize * 3, sqSize * 3))
 HorizontalLine = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\HorizontalLine.png"), (sqSize * 2, sqSize * 3))
 VerticalLine = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\VerticalLine.png"), (sqSize * 3, sqSize * 2))
+
+PlayButton = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\Play.png"), (sqSize - (OFFSET * 2), sqSize - (OFFSET * 2)))
+QuitButton = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\Quit.png"), (sqSize - (OFFSET * 2), sqSize - (OFFSET * 2)))
+
 Board = p.transform.scale(p.image.load(r"D:\Programming\Python\IDLE\TicTakToe\Images\Board.png"), (sqSize * 3, sqSize * 3))
 
 #   LOOP STARTS HERE
